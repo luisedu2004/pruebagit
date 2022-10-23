6a67066e6ad3085ef65193b9f04b5cfd042e2019
luisedu2004 <luiseduardogamer974@gmail.com>
2022-10-23 18:00:00 -0500

    Prueba 1.0

--- src/pruebagit/Pruebagit.java
+++ src/pruebagit/Pruebagit.java
@@ -14,6 +14,8 @@ public class Pruebagit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("HOLA");
         // TODO code application logic here
     }
     
